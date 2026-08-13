447787ae43c9f5d07de72d2e293bc48b5aa35b8d
Victor <victor28005@icloud.com>
2026-08-13 10:49:43 +0300

    чуть-чуть поменял расположение

--- Lab4/Lab4/Box/BoxApp.cpp
+++ Lab4/Lab4/Box/BoxApp.cpp
@@ -9,7 +9,6 @@
 #include <string>
 #include <stdexcept>
 
-
 using Microsoft::WRL::ComPtr;
 using namespace DirectX;
 using namespace DirectX::PackedVector;
@@ -532,8 +531,8 @@ void BoxApp::BuildShadersAndInputLayout()
 {
     HRESULT hr = S_OK;
     
-    mvsByteCode = d3dUtil::CompileShader(L"Shaders\\color.hlsl", nullptr, "VS", "vs_5_0");
-    mpsByteCode = d3dUtil::CompileShader(L"Shaders\\color.hlsl", nullptr, "PS", "ps_5_0");
+    mvsByteCode = d3dUtil::CompileShader(L"../../Assets/shaders/color.hlsl", nullptr, "VS", "vs_5_0");
+    mpsByteCode = d3dUtil::CompileShader(L"../../Assets/shaders/color.hlsl", nullptr, "PS", "ps_5_0");
 
     mInputLayout =
     {
